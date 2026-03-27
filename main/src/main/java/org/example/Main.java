58de3609f382e52294bc983aa1c78eda6cb2e935
piyush chauhan <piushhhh12@gmail.com>
2026-03-28 02:44:32 +0530

    added uc7

--- main/src/main/java/org/example/Main.java
+++ main/src/main/java/org/example/Main.java
@@ -3,18 +3,11 @@ package org.example;
 
 public class Main {
     static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello World!");
-        } else {
-            StringBuilder nameBuilder = new StringBuilder();
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-            if (nameBuilder.length() > 0) {
-                nameBuilder = new StringBuilder(nameBuilder.substring(0, nameBuilder.length() - 2));
-            }
-            System.out.println("Hello " + nameBuilder + "!");
+        String names = "World";
+        if (args.length > 0) {
+            names = String.join(", ", args);
         }
+        System.out.println("Hello " + names + "!");
 
 
 
